2be827fd22bacbf7ecdefa6e8243297ba8c87cc1
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-22 03:56:51 +0000

    update but not finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,8 +6,9 @@ public class PracticeProblem {
 
 	public static int lettersToFive(String sentence, char letter) {
 		int count = 0;
-		for (int i = 0; i < sentence.length(); ++i) {
-			if (sentence.charAt(i) == letter) ++count;
+		for (int i = 0; i < sentence.length(); ++i) { // i is the index of the letter
+			// ”aaa“ "a"
+			if (sentence.charAt(i) == letter) ++count; // if character at index i is the letter we want
 			if (count == 5) return i + 1;
 		}
 		return -1;
@@ -23,8 +24,14 @@ public class PracticeProblem {
 		return count;
 	}
 
-	
+
 	public static int oddSum(int first, int last) {
+		// int count = 0;
+		// for asldfjsalkdfj [5, 10] checks 4 5 6 7 8 9 10 11 12
+		// 11 5
+		// if (num%2 == 1) count += num;
+		// return count;
+
 		int actualFirst = Math.min(first, last), actualLast = Math.max(first, last);
 		if (actualFirst%2 == 0) {
 		++actualFirst;
